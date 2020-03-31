4173b033aeba3c6fdf46e266b05687281490a1f9
Danny Holland <holland.danny@gmail.com>
2020-03-31 09:54:48 -0600

    Fixed Page Title and added footer

--- app.js
+++ app.js
@@ -113,8 +113,7 @@ let el = document.querySelectorAll('.card');
 // console.log(el);
 
 btnAll.onclick = function() {
-	console.log(el);
-	el.classList.toggle('hidden');
+	alert("Show's All Options");
 
 	// let i;
 	// for (i = 0; i > el.length; i++) {
@@ -130,57 +129,3 @@ btnDelivery.onclick = function() {
 btnTakeout.onclick = function() {
 	alert("Get's Takeout Only Options");
 };
-
-// function toggler() {
-// 	console.log(el);
-// 	el.onclick = function() {
-// 		console.log('clicked');
-// 		el.classList.toggle('hidden');
-// 	};
-// }
-
-// function cardBuilder() {
-// 	let data = restaurants;
-// 	let card = document.getElementById('card');
-// 	let name = document.querySelector('h2');
-// 	// let content = '';
-// 	// for (i = 0; i < data.length; i++) {
-// 	// 	content += `<h2>` + data[i].restaurant`</h2>`();
-// 	// }
-// 	console.log(name);
-// 	console.log(card);
-// 	document.getElementById('card2').append = content;
-// }
-// cardBuilder();
-
-// function hideAndSHow() {
-// 	let el = document.getElementsByClassName('card');
-// 	console.log(el);
-// 	// let open = document.('open');
-// 	console.log(open);
-// 	// console.log(el.getAttribute('data-open'));
-// 	// let el = document.querySelectorAll('card');
-// 	// let hide = document.querySelectorAll('data-open');
-// 	// console.log(el);
-// 	// console.log(hide);
-// }
-// hideAndSHow();
-
-// el.onclick = function() {
-// 	console.log('clicked');
-// 	tog.classList.toggle('open');
-// };
-
-// let out = '';
-// 	var i;
-// 	for (i = 0; i < data.length; i++) {
-// 		out += '<h2>' + data[i].url + '</h2>' + arr[i].display + '</a><br>';
-// 	}
-// 	document.getElementById('id01').innerHTML = out;
-
-// let name = '';
-// 	let i;
-// 	for (i = 0; i < data.length; i++) {
-// 		name += `<h2>` + data[i].restaurant`</h2>`;
-// 		console.log(name);
-// 	}
